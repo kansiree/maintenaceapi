0ca173ea88d0e20070412f803c5f02662d2b17bc
kansiree <kansiree_print@hotmail.com>
2020-05-30 17:56:40 +0700

    rest api

--- go/src/github.com/kansiree/hellowold/main.go
+++ go/src/github.com/kansiree/hellowold/main.go
@@ -1,5 +1,37 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
 func main() {
-	println("hellowold")
+	handleRequest()
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome to the HomePage!")
+}
+func handleRequest() {
+	http.HandleFunc("/", homePage)
+	http.HandleFunc("/getAddress", getAddressBookAll)
+	http.ListenAndServe(":8080", nil)
+}
+
+type addressBook struct {
+	Firstname string
+	Lastname  string
+	Code      int
+	Phone     string
+}
+
+func getAddressBookAll(w http.ResponseWriter, r *http.Request) {
+	addBook := addressBook{
+		Firstname: "Chaiyarin",
+		Lastname:  "Niamsuwan",
+		Code:      1993,
+		Phone:     "0870940955",
+	}
+	json.NewEncoder(w).Encode(addBook) // (1)
 }
