55a22453f9d15d00968e77e7eec3b74228679b64
kansiree <kansiree_print@hotmail.com>
2020-09-13 01:42:20 +0700

    fe

--- dockerfile
+++ dockerfile
@@ -1,14 +1,19 @@
 FROM golang:1.13
+ENV GOPATH=/go
 
 # RUN mkdir /app
 ADD . /app
 WORKDIR /app
 COPY . .
+
 RUN go get github.com/go-sql-driver/mysql
 RUN go install github.com/go-sql-driver/mysql
+RUN go get github.com/gorilla/mux
+RUN go install github.com/gorilla/mux
+
 # RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o app .
 # RUN go build -o main
 ENTRYPOINT [ "go" ]
-CMD ["run","main/main.go"]
+CMD ["run","main.go "]
 
 
--- main.go
+++ main.go
@@ -12,6 +12,50 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Post struct {
+	Title string `json:"title"`
+	Body  string `json:"body"`
+}
+
+// 1) Struct for a Route
+type Route struct {
+	Name        string
+	Method      string
+	Pattern     string
+	HandlerFunc http.HandlerFunc
+}
+
+type BasicResponse struct {
+	Error   int             `json:"error"`
+	Message json.RawMessage `json:"message"`
+}
+
+type Masterdata []struct {
+	CreatedDate string `json:"created_date"`
+	FullName    string `json:"full_name"`
+	ID          string `json:"id"`
+}
+
+type Detaildata []struct {
+	ID                  string `json:"id"`
+	CreatedDate         string `json:"created_date"`
+	AircraftType        string `json:"aircraft_type"`
+	AircraftSN          string `json:"aircraft_sn"`
+	System              string `json:"system"`
+	PrimaryPilot        string `json:"primary_pilot"`
+	SecondaryPilot      string `json:"secondary_pilot"`
+	Recorder            string `json:"recorder"`
+	Trouble             string `json:"trouble"`
+	TechnicalOrder      string `json:"technical_order"`
+	TroubleShooting     string `json:"trouble_shooting"`
+	Replace             string `json:"replace"`
+	Name                string `json:"name"`
+	PartNumber          string `json:"part_number"`
+	SerailNumberRemove  string `json:"serail_number_remove"`
+	SerailNumberinstall string `json:"serail_number_install"`
+	Remark              string `json:"remark"`
+}
+
 type Masters []Masterdata
 
 type Routes []Route
@@ -56,19 +100,19 @@ func handleRequest() *mux.Router {
 		Route{
 			"getMasterAircraft",
 			"GET",
-			"/",
+			"/getMasterAircraft",
 			getMasterAircraft,
 		},
 		Route{
 			"getMasterTechnicalOrder",
 			"GET",
-			"/",
+			"/getMasterTechnicalOrder",
 			getMasterTechnicalOrder,
 		},
 		Route{
 			"getDetail",
 			"GET",
-			"/",
+			"/getDetail",
 			getDetail,
 		},
 	}
@@ -104,11 +148,10 @@ func getMasterSystem(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		println(response)
 		w.Header().Set("Content-type", "application/json; charset=UTF-8;")
 		json.NewEncoder(w).Encode(BasicResponse{
 			0,
-			(js),
+			js,
 		})
 	}
 
@@ -126,8 +169,11 @@ func getMasterAircraft(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+		w.Header().Set("Content-type", "application/json; charset=UTF-8;")
+		json.NewEncoder(w).Encode(BasicResponse{
+			0,
+			js,
+		})
 	}
 
 }
@@ -144,8 +190,11 @@ func getMasterTechnicalOrder(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+		w.Header().Set("Content-type", "application/json; charset=UTF-8;")
+		json.NewEncoder(w).Encode(BasicResponse{
+			0,
+			js,
+		})
 	}
 
 }
@@ -165,8 +214,11 @@ func getDetail(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(js)
+		w.Header().Set("Content-type", "application/json; charset=UTF-8;")
+		json.NewEncoder(w).Encode(BasicResponse{
+			0,
+			js,
+		})
 	}
 }
 
