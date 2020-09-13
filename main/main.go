b3a52a2953fcb2fc0329852398ff54a685ad00b1
kansiree <kansiree_print@hotmail.com>
2020-09-13 23:58:09 +0700

    post data detail aricraft

--- main/main.go
+++ main/main.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"encoding/json"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
@@ -28,6 +30,11 @@ type Route struct {
 type BasicResponse struct {
 	Error   int             `json:"error"`
 	Message json.RawMessage `json:"message"`
+}
+
+type BasicResponseStringMessage struct {
+	Error   int    `json:"error"`
+	Message string `json:"message"`
 }
 
 type Masterdata []struct {
@@ -56,35 +63,14 @@ type Detaildata []struct {
 	Remark              string `json:"remark"`
 }
 
-type Masters []Masterdata
-
 type Routes []Route
 
-func getIndex(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello world!")
-}
-
-func homePage(w http.ResponseWriter, r *http.Request) {
-
-	err := r.ParseForm()
-	if err != nil {
-		panic(err)
-	}
-	id := r.FormValue("id")
-	log.Println(id)
-	defer r.Body.Close()
-}
 func handleRequest() *mux.Router {
 
 	router := mux.NewRouter().StrictSlash(true)
 
 	var routes = Routes{
-		Route{
-			"getIndex",
-			"GET",
-			"/",
-			getIndex,
-		},
+
 		Route{
 			"home",
 			"GET",
@@ -115,6 +101,12 @@ func handleRequest() *mux.Router {
 			"/getDetail",
 			getDetail,
 		},
+		Route{
+			"insertDetail",
+			"POST",
+			"/insertDetail",
+			insertDetail,
+		},
 	}
 
 	for _, route := range routes {
@@ -136,10 +128,25 @@ func main() {
 	)
 }
 
+func getIndex(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world!")
+}
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	err := r.ParseForm()
+	if err != nil {
+		panic(err)
+	}
+	id := r.FormValue("id")
+	log.Println(id)
+	defer r.Body.Close()
+}
+
 func getMasterSystem(w http.ResponseWriter, r *http.Request) {
 
 	var masters Masterdata
-	response, err := getJSON("SELECT * FROM t016ffukzsi0y5ie.master_system")
+	response, err := selecDataReturnJsonFormat("SELECT * FROM t016ffukzsi0y5ie.master_system")
 	if err != nil {
 		log.Fatalln(err)
 	} else {
@@ -160,7 +167,7 @@ func getMasterSystem(w http.ResponseWriter, r *http.Request) {
 func getMasterAircraft(w http.ResponseWriter, r *http.Request) {
 
 	var masters Masterdata
-	response, err := getJSON("SELECT * FROM t016ffukzsi0y5ie.master_aircraft")
+	response, err := selecDataReturnJsonFormat("SELECT * FROM t016ffukzsi0y5ie.master_aircraft")
 	if err != nil {
 		log.Fatalln(err)
 	} else {
@@ -181,7 +188,7 @@ func getMasterAircraft(w http.ResponseWriter, r *http.Request) {
 func getMasterTechnicalOrder(w http.ResponseWriter, r *http.Request) {
 
 	var masters Masterdata
-	response, err := getJSON("SELECT * FROM t016ffukzsi0y5ie.master_technical_order")
+	response, err := selecDataReturnJsonFormat("SELECT * FROM t016ffukzsi0y5ie.master_technical_order")
 	if err != nil {
 		log.Fatalln(err)
 	} else {
@@ -200,12 +207,41 @@ func getMasterTechnicalOrder(w http.ResponseWriter, r *http.Request) {
 }
 
 func insertDetail(w http.ResponseWriter, r *http.Request) {
-
+	var detailData Detaildata
+	body, err := ioutil.ReadAll(io.LimitReader(r.Body, 1048576))
+	if err != nil {
+		panic(err)
+	}
+	if err := r.Body.Close(); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal(body, &detailData); err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(422)
+		if err := json.NewEncoder(w).Encode(err); err != nil {
+			panic(err)
+		}
+	}
+	db, err := connectDB()
+	//	response, err := queryDB("insert into maintanace_detail values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?) ")
+	stmt, err := db.Prepare("insert into blogs values(?,?,?,?,?)")
+	if err != nil {
+		panic(err)
+	}
+	_, err = stmt.Exec("", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "")
+	if err != nil {
+		panic(err.Error())
+	}
+	w.Header().Set("Content-type", "application/json; charset=UTF-8;")
+	json.NewEncoder(w).Encode(BasicResponseStringMessage{
+		0,
+		"success",
+	})
 }
 
 func getDetail(w http.ResponseWriter, r *http.Request) {
 	var dataDetail Detaildata
-	response, err := getJSON("SELECT * FROM t016ffukzsi0y5ie.maintenace_detail")
+	response, err := selecDataReturnJsonFormat("SELECT * FROM t016ffukzsi0y5ie.maintenace_detail")
 	if err != nil {
 		log.Fatalln(err)
 	} else {
@@ -231,15 +267,20 @@ func getPort() string {
 	return ":" + port
 }
 
-func getJSON(sqlString string) (string, error) {
-	db, err := sql.Open("mysql", "sz0debklevf8wjhf:gu2af8swu50tjc3k@tcp(u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/t016ffukzsi0y5ie")
+func connectDB() (*sql.DB, error) {
+	return sql.Open("mysql", "sz0debklevf8wjhf:gu2af8swu50tjc3k@tcp(u3r5w4ayhxzdrw87.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306)/t016ffukzsi0y5ie")
+}
+
+func queryDB(sqlString string) (*sql.Rows, error) {
+	db, err := connectDB()
 	if err != nil {
 		log.Fatal(err)
 	}
-	rows, err := db.Query(sqlString)
-	if err != nil {
-		return "", err
-	}
+	return db.Query(sqlString)
+}
+
+func selecDataReturnJsonFormat(sqlString string) (string, error) {
+	rows, err := queryDB(sqlString)
 	defer rows.Close()
 	columns, err := rows.Columns()
 	if err != nil {
@@ -270,7 +311,7 @@ func getJSON(sqlString string) (string, error) {
 		}
 		tableData = append(tableData, entry)
 	}
-
+	fmt.Println(tableData)
 	jsonData, err := json.Marshal(tableData)
 	if err != nil {
 		return "", err
